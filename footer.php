dd88575f9bf4bcce0507c14d5f970a1e9204f1e0
boguiemmanuel <94707939+boguiemmanuel@users.noreply.github.com>
2022-12-15 14:52:00 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,83 @@
+<!-- footer -->
+<footer>
+          <div class="services_list">
+              <div class="service">
+                  <h2>reseaux</h2>
+                  <p>########</p>
+                  <p>#####"""#</p>
+              </div>
+
+              <div class="service">
+                <h2>Adresses</h2>
+                <p>yopougon toits rouge</p>
+                <p>Abidjan</p>
+            </div>
+            <div class="service">
+                <h2>E-mail</h2>
+                <p>bodjuiemmanuel388@gmai.com</p>
+                <p>bogui@gmail.com</p>
+            </div>
+            <div class="service">
+                <h2>Numbers</h2>
+                <p>+225 0747860451</p>
+                <p>+255 0556833799</p>
+            </div>
+            
+            <hr>
+          </div>
+
+          <p class="footer_text">Réalisé par <span>bogui emmanuel</span> | Tous les droits sont réservés.</p>
+      </footer>
+    
+  <style>
+
+    /* footer CSS */
+
+footer {
+    background-color: rgba(0,0,0,0.05);
+}
+footer .services_list {
+    padding: 10px 10%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+}
+.service img {
+    width: 40px;
+    background-color: #27ae60;
+    padding: 10px;
+    border-radius: 50%;
+    margin-bottom: 10px;
+
+}
+.service {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 25px auto;
+}
+.service  p {
+    color: #333;
+}
+hr {
+    background-color: #ccc;
+    border: 0;
+    height: 2px;
+    width: 100%;
+}
+.footer_text {
+    text-align: center;
+    font-size: 15px;
+    padding: 8px 0;
+}
+.footer_text span {
+    color: #27ae60;
+}
+.toggle_menu {
+    display: none;
+}
+
+  </style>
+ 
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+   <!-- <link rel="stylesheet" href="style1.css">-->
+    <title>portfolio</title>
+</head>
+<body>
+   
+    <div class="header">
+              <img src="images/4.jpg" class="logo" alt="">
+            <div class="menu">
+                <ul>
+                   <li><a href="">home</a></li>
+                   <li><a href="#Apropos">Apropos</a></li>
+                   <li><a href="#projet">projet</a></li>
+                   <li><a href="#contact">contact</a></li>
+                </ul>
+            </div>
+            <img src="images/10.jpg"  class="images" alt="">
+            <h1 class="infor"> Développeur <samp style="color: blue;">full-stack</samp> </h1>
+    </div>
+    <marquee  class="mar"> l'informatique une passion</marquee>
+     <h1 style="text-align: center; font-size:80px;"> Mon portfolio</h1>
+     <p style="font-size:20px; text-align :center" class="text1">a avenir Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit reprehenderit molestias quos. Eius maxime hic repellendus tempora enim! Doloremque inventore ratione porro qui vel cum nesciunt consequuntur repudiandae quasi at?
+     Eaque tempore nostrum ducimus dignissimos aliquam, quis ipsum, nisi excepturi ea veniam sapiente soluta eligendi quibusdam eius quam aperiam, beatae eum qui tenetur obcaecati? Expedita assumenda dignissimos magnam deleniti tempora.
+     Obcaecati minus voluptatum a, ea quasi sint numquam dignissimos excepturi suscipit, ullam quidem odit nemo quam laborum soluta nihil sapiente saepe tenetur optio, accusantium iure aliquam magnam? Architecto, sunt iusto?
+     Error laudantium, ipsa ea exercitationem officia odit suscipit voluptas pariatur a eum hic, recusandae impedit vel? Sint at magnam aliquid culpa impedit tempore, numquam nulla odit facere tempora veniam dolorum?
+     Delectus in odio tempore corporis quas ducimus obcaecati harum. Eveniet nihil alias ipsam laboriosam cumque laborum, veniam recusandae saepe est, ducimus optio, maiores dolorum eius eum amet nesciunt aperiam nobis.
+     Atque et voluptatibus, fugit aspernatur commodi, non rerum harum accusantium, dolorem laudantium perspiciatis unde veniam omnis? Eaque aliquam, eos accusantium illo consequatur similique, itaque odio ut neque, sunt omnis. Sint.
+     Officia dolores consequatur odit corrupti reiciendis nihil vitae maiores dicta vero aliquid atque delectus eius ex tempore sunt magni quam accusamus, doloribus adipisci eveniet laboriosam? Alias at possimus sequi earum?
+     Eum sequi tempora veniam aut voluptate voluptates quod itaque quo unde ea et, culpa porro accusantium nobis earum quam nesciunt error odit magnam tenetur excepturi illum nostrum. Fuga, pariatur adipisci.</p>
+
+          <h1 style="font-size: 80px; text-align :center;">A propos de moi</h1>
+          <p class="text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero, recusandae. 
+            Impedit a illum, dolorem odio commodi possimus et deleniti facere!
+             Dicta magni, sapiente saepe vel reprehenderit eaque officia beatae dolores.
+             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor tempore recusandae, similique dignissimos autem molestias numquam sunt quibusdam quae optio natus vel doloribus facere error maxime, porro excepturi fuga. Rem.
+             Expedita aut vel, blanditiis accusamus perspiciatis ab dignissimos incidunt molestias deserunt. Officiis sint totam culpa beatae voluptatibus dolorum, eaque, facere quasi nobis in aut harum illum doloribus deserunt eligendi tempore?
+             Nisi ad accusantium nulla libero, quibusdam eum provident expedita reiciendis adipisci magnam et optio, molestiae nobis maiores dolor harum eveniet in quod molestias obcaecati. Voluptas voluptatem voluptatum incidunt tenetur natus!
+             Velit, voluptatem architecto ullam laudantium tenetur esse rerum optio nostrum fugit veniam eum voluptatum in sit eaque eos? Unde minus deleniti cupiditate ipsam quasi dolore dignissimos quae. Quis, necessitatibus soluta!
+             Deserunt, delectus, ducimus voluptatibus libero quidem sint rem, officia velit aperiam eveniet odit quaerat consequatur? Est quaerat sequi expedita aliquid ex beatae, reprehenderit voluptatibus qui quo? Perferendis maxime ut adipisci.
+             Dolorem veritatis cupiditate excepturi quia, doloremque voluptatibus debitis ipsum quasi consequuntur praesentium distinctio. Libero illum, neque, ipsum deserunt inventore nesciunt voluptatibus, id repellat provident perspiciatis sed accusantium dolor nobis rerum!
+             Sit, laboriosam recusandae officia inventore architecto quas aut culpa voluptatum, ea, reiciendis tenetur unde dolore atque at itaque? Deleniti et rerum tempore facere rem error, officia voluptatum recusandae pariatur doloribus?
+             Illo quo laboriosam eaque molestias hic mollitia labore est repellendus, dignissimos in ad vitae quidem nemo itaque voluptatem ipsum dolores unde animi nisi commodi enim quisquam. Ullam cum libero ut?
+             Placeat, laboriosam ipsum. Quibusdam suscipit libero odio quam eum sunt, harum adipisci provident molestias quaerat laboriosam ad earum ex! Nulla eligendi numquam quam doloremque error nostrum, quos deleniti beatae officiis!
+             Sed explicabo vitae, dolores sunt fugiat ipsam eius ratione numquam quisquam, itaque dolorum quaerat maxime qui libero! Reiciendis quas aut soluta, repudiandae eveniet libero hic maiores ullam praesentium officiis sequi.
+
+          </p>
+        
+
+    <?php include('footer.php'); ?>
+</body>
+</html>
